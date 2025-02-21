97ae7051c7f443cebe1b1e55a6a5970468793231
Michael-Andryeer <andryeerm@gmail.com>
2025-02-21 16:17:02 -0300

    Correção de bug no gif

--- src/assets/illustrations/career-hero-illustration.tsx
+++ src/assets/illustrations/career-hero-illustration.tsx
@@ -2,16 +2,47 @@ import { m } from 'framer-motion';
 import { memo } from 'react';
 
 import Box, { BoxProps } from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+
+import { useHoverParallax } from 'src/hooks/use-hover-parallax';
 
 import Image from 'src/components/image';
+import SvgColor from 'src/components/svg-color';
+
+import Icon from './pattern/icon';
+import Label from './pattern/label';
+import Pattern01 from './pattern/pattern-01';
+import Pattern02 from './pattern/pattern-02';
+
+// ----------------------------------------------------------------------
+
+const stylesIcon = {
+  width: 40,
+  height: 40,
+  color: 'common.black',
+};
+
+// ----------------------------------------------------------------------
 
 function CareerHeroIllustration({ sx, ...other }: BoxProps) {
+  const theme = useTheme();
+
+  const { offsetX, offsetY, onMouseMoveHandler, onMouseLeaveHandler } = useHoverParallax();
+
+  const BLUE = theme.palette.info.main;
+
+  const GREEN = theme.palette.success.main;
+
+  const YELLOW = theme.palette.warning.main;
+
   return (
     <Box
       component={m.div}
+      onMouseMove={onMouseMoveHandler}
+      onMouseLeave={onMouseLeaveHandler}
       sx={{
-        width: 640,
-        height: 640,
+        width: 564,
+        height: 564,
         display: 'flex',
         alignItems: 'center',
         position: 'relative',
@@ -20,24 +51,159 @@ function CareerHeroIllustration({ sx, ...other }: BoxProps) {
       }}
       {...other}
     >
-      <Box
-        sx={{
-          position: 'absolute',
-          bottom: 16,
-          zIndex: 10,
-          width: 640,
-        }}
-      >
-        <Image
-          alt="Character Animation"
-          src="/assets/images/marketing/Oh neymar.gif"
+      <>
+        {/* GIF central */}
+        <Box
           sx={{
-            width: '100%',
-            height: 'auto',
-            maxWidth: 640,
+            position: 'absolute',
+            bottom: 16,
+            zIndex: 0,
+            width: 400,
           }}
-        />
-      </Box>
+        >
+          <Image
+            alt="Character Animation"
+            src="/assets/images/marketing/Oh neymar.gif"
+            sx={{
+              width: '100%',
+              height: 'auto',
+            }}
+          />
+
+        </Box>
+
+        {/* Soluções Empresariais */}
+        <Box
+          sx={{
+            top: 170,
+            zIndex: 9,
+            position: 'absolute',
+            transform: 'translateX(-125px) rotate(-15deg)',
+          }}
+        >
+          <m.div style={{ y: offsetY(-50) }}>
+            <Label
+              text="Soluções Empresariais"
+              icon={
+                <Image
+                  alt="Soluções Empresariais"
+                  src="/assets/icons/service/solucoes.svg"
+                  sx={{ width: 48, height: 48 }}
+                />
+              }
+            />
+          </m.div>
+        </Box>
+
+        {/* SEO */}
+        <Box
+          sx={{
+            position: 'absolute',
+            transform: 'translate(175px, 90px) rotate(15deg)',
+          }}
+        >
+          <m.div style={{ x: offsetX(80), y: offsetY(80) }}>
+            <Label
+              text="SEO"
+              icon={
+                <Image
+                  alt="SEO"
+                  src="/assets/icons/service/seo.svg"
+                  sx={{ width: 48, height: 48 }}
+                />
+              }
+            />
+          </m.div>
+        </Box>
+
+        {/* Atendimento Personalizado */}
+        <Box
+          sx={{
+            position: 'absolute',
+            transform: 'translate(170px, -110px) rotate(15deg)',
+          }}
+        >
+          <m.div style={{ y: offsetY(80) }}>
+            <Label
+              text="Atendimento Personalizado"
+              icon={
+                <Image
+                  alt="Atendimento Personalizado"
+                  src="/assets/icons/service/atendimento.svg"
+                  sx={{ width: 48, height: 48 }}
+                />
+              }
+            />
+          </m.div>
+        </Box>
+
+        {/* Software */}
+        <Box
+          sx={{
+            zIndex: 10,
+            bottom: 160,
+            position: 'absolute',
+            transform: 'translateX(-110px)',
+          }}
+        >
+          <m.div style={{ y: offsetY(-60) }}>
+            <Label
+              text="Software"
+              icon={
+                <Image
+                  alt="software development"
+                  src="/assets/icons/ic_software_development.svg"
+                  sx={{ width: 48, height: 48 }}
+                />
+              }
+            />
+          </m.div>
+        </Box>
+
+        {/* Icon */}
+        <Box sx={{ position: 'absolute', top: 16, transform: 'translateX(20px)' }}>
+          <m.div style={{ x: offsetX(50), y: offsetY(50) }}>
+            <Icon
+              color={YELLOW}
+              content={<SvgColor src="/assets/icons/ic_creativity.svg" sx={{ ...stylesIcon }} />}
+            />
+          </m.div>
+        </Box>
+
+        {/* Icon */}
+        <Box sx={{ position: 'absolute', bottom: 16, transform: 'translateX(40px)' }}>
+          <m.div style={{ x: offsetX(-60), y: offsetY(60) }}>
+            <Icon
+              color={GREEN}
+              content={
+                <SvgColor src="/assets/icons/ic_marketing_bullhorn.svg" sx={{ ...stylesIcon }} />
+              }
+            />
+          </m.div>
+        </Box>
+
+        {/* Icon */}
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: 220,
+            transform: 'translateX(-220px)',
+          }}
+        >
+          <m.div style={{ x: offsetX(70), y: offsetY(70) }}>
+            <Icon
+              color={BLUE}
+              content={
+                <SvgColor src="/assets/icons/ic_customer_service.svg" sx={{ ...stylesIcon }} />
+              }
+            />
+          </m.div>
+        </Box>
+      </>
+
+      <Pattern01 />
+
+      <Pattern02 />
     </Box>
   );
 }
